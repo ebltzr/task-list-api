c726d305b72dce7f04dc7e44af579028540a533b
Edith <mxedithbaltazar@gmail.com>
2023-05-03 15:11:47 -0700

    added Task model

--- app/models/task.py
+++ app/models/task.py
@@ -3,3 +3,6 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(80))
+    description=db.Column(db.String(100))
+    completed_at=db.Column(db.DateTime,nullable=True)
