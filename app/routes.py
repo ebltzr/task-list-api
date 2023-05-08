7466764b0a64394b2bb14b8b1be303a380393c76
Edith <mxedithbaltazar@gmail.com>
2023-05-08 13:55:30 -0700

    created route mark-complete, created route mark_incomplete

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, abort, make_response, request
 from app import db
 from app.models.task import Task
+from datetime import datetime
 
 
 def validate_task(task_id):
@@ -12,25 +13,23 @@ def validate_task(task_id):
     task = Task.query.get(task_id)
     
     # 4. 
-    return task if task else abort(make_response({'msg':
-        f'No task with id: {task_id}'}, 404))
+    return task if task else abort(make_response({'msg': f'No task with id: {task_id}'}, 404))
 
 task_bp = Blueprint("tasks" ,__name__, url_prefix="/tasks")
 
 
-# CREATE A TASK --- PASSED
 @task_bp.route("", methods=['POST'])
 def create_task():
     request_body = request.get_json()
     
-    # task must contain title/description --- PASSED
     if 'title' not in request_body or 'description' not in request_body:
         return {"details": "Invalid data"}, 400
     
     new_task = Task(
         title = request_body['title'],
         description = request_body['description'],
-        completed_at = request_body['completed_at'] if 'completed_at' in request_body else None
+        completed_at = request_body['completed_at'] if 'completed_at' in 
+                        request_body else None
     )
     
     db.session.add(new_task)
@@ -39,28 +38,13 @@ def create_task():
     return {"task": new_task.to_dict()}, 201
     
 
-# GET TASKS: NO SAVED TASKS --- PASSED
-# GET TASKS: ONE SAVED TASK --- PASSED
-# SORTING TASKS: BY TITLE, ASC --- PASSED
-# SORTING TASKS: BY TITLE, DESC --- PASSED
 @task_bp.route('', methods=['GET'])
 def get_tasks():
-#     response = []
-#     title_query = request.args.get("title")
-    
-#     if title_query is None:
-#         tasks = Task.query.all()
-#     else:
-#         tasks = Task.query.filter_by(title=title_query).all()
-        
-#     for task in tasks:
-#         response.append(task.to_dict())
-        
-#     return jsonify(response), 200   
-
     response = []
+    # https://www.javatpoint.com/multi-value-query-parameters-with-flask
     sort_tasks = request.args.get("sort")
     if sort_tasks == "asc":
+        # https://sparkbyexamples.com/pyspark/pyspark-orderby-and-sort-explained/
         tasks = Task.query.order_by(Task.title.asc()).all()
     elif sort_tasks == "desc":
         tasks = Task.query.order_by(Task.title.desc()).all()
@@ -73,7 +57,6 @@ def get_tasks():
     return jsonify(response), 200
 
 
-# GET ONE TASK: ONE SAVED TASK  --- PASSED
 @task_bp.route("/<task_id>", methods=['GET'])
 def get_task(task_id):
     task = validate_task(task_id)
@@ -83,7 +66,6 @@ def get_task(task_id):
         }, 200
 
 
-#UPDATE TASK  --- PASSED
 @task_bp.route("/<task_id>", methods=['PUT'])
 def update_task(task_id):
     task = validate_task(task_id)
@@ -98,7 +80,6 @@ def update_task(task_id):
             "task": task.to_dict()
         }, 200
 
-# DELETE TASK  --- PASSED
 @task_bp.route('/<task_id>', methods=['DELETE'])
 def delete_task(task_id):
     task_deleted = validate_task(task_id)
@@ -110,4 +91,28 @@ def delete_task(task_id):
         "details":
             f'Task {task_id} \"{task_deleted.title}\" successfully deleted'
         }
+    
+
+# WAVE 3 - MARK COMPLETE --- PASSED
+@task_bp.route('/<task_id>/mark_complete', methods=['PATCH'])
+def mark_complete(task_id):
+    task = validate_task(task_id)
+    task.completed_at= datetime.now()
+
+    db.session.commit()
+    return {
+            "task": task.to_dict()
+        }, 200
+    
+
+# WAVE 3 MARK INCOMPLETE --- PASSED
+@task_bp.route('/<task_id>/mark_incomplete', methods=['PATCH'])
+def mark_incomplete(task_id):
+    task = validate_task(task_id)
+    task.completed_at = None
+    
+    db.session.commit()
+    return {
+            "task": task.to_dict()
+        }, 200
     
